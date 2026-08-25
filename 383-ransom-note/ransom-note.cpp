cf2de0445d2cefe261f8f287506678a1dd8c575f
Vedhanth M <vedhanthmanju@gmail.com>
2026-08-25 14:17:36 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (98.22%) - LeetSync

---
+++ 383-ransom-note/ransom-note.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        if(ransomNote.length() > magazine.length()){
+            return false;
+        }
+        int arr[26] = {0};
+        for(int i = 0; i < ransomNote.length(); i++){
+            arr[ransomNote[i] - 97]++;
+        }
+        for(int i = 0; i < magazine.length(); i++){
+            arr[magazine[i] - 97]--;
+        }
+        for(int i = 0; i < 26; i++){
+            if(arr[i] > 0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
